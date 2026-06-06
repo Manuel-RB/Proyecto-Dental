02f5326a35d5bea59d952bc83aca2edc6ad4595b
Manuel Ramirez <marabo98@hotmail.com>
2026-06-05 23:28:25 -0500

    Cerrar modal

--- js/script.js
+++ js/script.js
@@ -78,4 +78,46 @@ fechaInput.addEventListener("change", () => {
     }
 
     alert(`Cita agendada para ${fecha} a las ${horaSeleccionada}`);
+    modal.style.display = "none";
+    limpiarFormulario()
  });
+
+ // ----------------------------------- Accion cerrar modal ------------------------------------
+
+ const cerrarModal = document.getElementById("cerrar-modal");
+
+ cerrarModal.addEventListener("click", () => { //Funcion para cerrar el modal dando click en la X
+    modal.style.display = "none";
+    limpiarFormulario()
+ });
+
+ modal.addEventListener("click", (event) => { //Cierra el modal dando click fuera de el
+    if(event.target === modal){
+        modal.style.display = "none";
+        limpiarFormulario()
+    }
+ });
+
+ // -------------------- Funcion para borrar elementos guardados del modal ---------------------
+ 
+ function limpiarFormulario() {
+    fechaInput.value = "";
+    horaSeleccionada = null;
+    document.getElementById(
+        "fecha-resumen"
+    ).textContent =
+    "Fecha: No seleccionada";
+    document.getElementById(
+        "hora-resumen"
+    ).textContent =
+    "Hora: No seleccionada";
+    const botones =
+    document.querySelectorAll(
+        "#horarios button"
+    );
+    botones.forEach(boton => {
+        boton.classList.remove(
+            "hora-seleccionada"
+        );
+    });
+}
